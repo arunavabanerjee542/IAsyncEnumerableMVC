44b8365b3183964eb95dee53000354660cc2b673
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2021-02-11 06:12:46 -0800

    Add project files.

---
+++ IAsyncEnumerableMVC/Models/WebServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerableMVC.Models
+{
+    public static class WebServer
+    {
+     
+        public static async IAsyncEnumerable<string> GetWebsiteContent()
+        {
+            var httpClient = new HttpClient();
+
+            foreach(var website in WebSites.GetListOfWebSites())
+            {
+               var response =  await httpClient.GetAsync(website);
+                yield return await response.Content.ReadAsStringAsync() ;
+
+            }
+
+        }
+
+
+    }
+}
---
+++ IAsyncEnumerableMVC/Models/WebSites.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerableMVC.Models
+{
+    public static class WebSites
+    {
+
+        public static IEnumerable<string> GetListOfWebSites()
+        {
+            return new List<string>()
+            {
+                "https://google.com",
+                "https://microsoft.com",
+                "https://www.salesforce.com"
+            };
+
+        }
+
+
+
+    }
+}
